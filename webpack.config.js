d1f5a4bd39013b566f50bbb8afde02b8850113b7
George Wilkins <gwilkins@apprissretail.com>
2020-01-20 21:19:34 -0600

    all updates

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+const postCSSPlugins= [
+    require('postcss-import'),
+    require('postcss-simple-vars'), 
+    require('postcss-nested'), 
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js', 
+    output: {
+        filename: 'bundled.js', 
+        path: path.resolve(__dirname, 'app')
+    }, 
+
+    mode: 'development', 
+    //watch: true, //not needed since we have webpack dev server 
+
+    devServer: {
+        before: function(app, server){
+            server._watch('./app/**/*.html');
+        }, 
+        contentBase: path.join(__dirname, 'app'), 
+        hot: true,
+        port: 3000, 
+        host: '0.0.0.0'
+
+    },
+
+    module: {
+        rules:[
+            {
+                test: /\.css$/i, 
+                use: [
+                    'style-loader', 
+                    'css-loader?url=false', 
+                    {
+                        loader: 'postcss-loader', 
+                        options: {plugins: postCSSPlugins}
+                    }
+                ]
+            }
+
+
+
+        ]
+
+
+
+
+
+    }
+
+    
+
+
+
+
+
+}
